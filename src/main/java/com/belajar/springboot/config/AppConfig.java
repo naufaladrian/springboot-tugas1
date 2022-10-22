9d414746d0f2131e8f594578eb456faf9b562f13
naufaladrian <naufal.adrian904@gmail.com>
2022-10-22 10:57:21 +0700

    update AppConfig

---
+++ src/main/java/com/belajar/springboot/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.belajar.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class AppConfig extends WebMvcConfigurerAdapter  {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").maxAge(3600);
+    }
+}
